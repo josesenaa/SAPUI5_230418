be87d3d6310a549e311440e856fb5b92714bc44d
Jose Sena Alonzo <josesenaa@gmail.com>
2023-02-08 04:30:13 +0000

    yes

--- First/invoices/webapp/Controller/Main.controller.js
+++ First/invoices/webapp/Controller/Main.controller.js
@@ -1,16 +1,30 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast'
+    'sap/m/MessageToast',
+    'logaligroup/invoices/model/models',
+    'sap/ui/model/resource/ResourceModel'
+
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, models, ResourceModel) {
         'use strict';
         return Controller.extend("logaligroup.invoices.Controller.Main", {
+            onInit: function () {
+
+                this.getView().setModel(models.CreateRecipient());
+
+                var i18nModel = new ResourceModel({ bundleName : "logaligroup.invoices.i18n.i18n"});
+                this.getView().setModel(i18nModel, "i18n");
+            },
+
             onShowHello: function () {
-                MessageToast.show("Hello World!!!");
+               var oBundle = this.getView().getModel("i18n").getResourceBundle();
+               var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+               var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+                MessageToast.show(sMsg);
             }
         });
     });
