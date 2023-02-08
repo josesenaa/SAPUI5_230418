2a5ec29302a9ba2e5f2e6a1be4b0db4b2869a78f
Jose Sena Alonzo <josesenaa@gmail.com>
2023-02-08 03:18:39 +0000

    yes

--- First/invoices/webapp/Controller/Main.controller.js
+++ First/invoices/webapp/Controller/Main.controller.js
@@ -1,15 +1,16 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], 
-/**
- * 
- * @param {typeof sap.ui.core.mvc.Controller} Controller 
- */
-function(Controller) {
-    'use strict';
-    return Controller.extend("logaligroup.invoices.controller.Main", {
-       onShowHello: function() {
-          alert("Hello World!!!");
-       } 
+    'sap/ui/core/mvc/Controller',
+    'sap/m/MessageToast'
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     */
+    function (Controller, MessageToast) {
+        'use strict';
+        return Controller.extend("logaligroup.invoices.Controller.Main", {
+            onShowHello: function () {
+                MessageToast.show("Hello World!!!");
+            }
+        });
     });
-});
